9012668b6cb1ed43ab62388e366a25cf2effee13
AD JOY <adjooy@gmail.com>
2024-08-21 08:23:42 +0600

    updated

---
+++ src/main/kotlin/grade_calculator_with_if_else.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val marks = print("Enter your marks: ").let { readln().toInt() }
+    if (marks > 90)
+         println("Your Grade is A+")
+    else if (marks > 80)
+        println("Your Grade is A")
+    else if (marks > 70)
+        println("Your Grade is A-")
+    else if (marks > 60)
+        println("Your Grade is B+")
+    else if (marks > 50)
+        println("Your Grade is B-")
+    else if (marks > 40)
+        println("Your Grade is C")
+    else
+        println("Your failed")
+
+
+
+}
