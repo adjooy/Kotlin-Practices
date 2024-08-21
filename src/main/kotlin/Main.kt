0ef3b74ec5debfb21716b8e92f8ce1ccd5478a81
AD JOY <adjooy@gmail.com>
2024-08-21 07:38:14 +0600

    Getting Input from user

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main(){
-    println("Hello Joy")
+
+    // For input Strings
+    print("Enter your name: ")
+    val name = readln()
+
+    // For input Integer
+    print("Enter your age: ")
+    val age = readln().toInt()
+
+    //Note: We can use readln(), readline() or readlnOrNull() for user input
+
+    println("User name is: $name and user age is: $age")
 }
